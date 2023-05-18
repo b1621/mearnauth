3525d3347ce8cc375e0a2e81ed7a137327e924df
b1621 <bewuket1621@gmail.com>
2023-05-18 21:35:22 +0300

    second commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,39 +1,46 @@
-import asyncHandler from 'express-async-handler'
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access public
-const authUser = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:'Auth User'})
-})
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+});
 
 // @desc Register a new user
 // route POST /api/users
 // @access public
-const registerUser = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:'Register User'})
-})
+const registerUser = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "Register User" });
+});
 
 // @desc Logout user
 // route POST /api/users/logout
 // @access public
-const logoutUser = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:'logout User'})
-})
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "logout User" });
+});
 
 // @desc   Get user profile
 // route   Get /api/users/logout
 // @access private
-const getUserProfile = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:'get User profile'})
-})
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "get User profile" });
+});
 
 // @desc   Update user profile
 // route   PUT /api/users/logout
 // @access private
-const updateUserProfile = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:'Update User profile'})
-})
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User profile" });
+});
 
-export {authUser,registerUser,logoutUser,getUserProfile,updateUserProfile}
-
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import {notFound,errorHandler} from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js';
-const port = process.env.PORT || 5000
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+const port = process.env.PORT || 5000;
 
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
 
-connectDB()
+connectDB();
 const app = express();
 
-app.use('/api/users',userRoutes)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=> res.send('server is ready'))
+app.use("/api/users", userRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.get("/", (req, res) => res.send("server is ready"));
 
-app.listen(port,()=>console.log(`server started on port ${port}`))
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server started on port ${port}`));
